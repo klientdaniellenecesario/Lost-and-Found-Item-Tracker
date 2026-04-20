665a2def310b35705d091fd5a555cd2c1e44202d
Jazelle Tamayo <jazellet5@gmail.com>
2026-04-20 10:04:08 +0800

    Fixed HomeController to use GetInt32 for UserId session value and query database for dashboard stats. Removed dependency on static items list.

--- LostAndFoundTracker/Controllers/HomeController.cs
+++ LostAndFoundTracker/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using System.Collections.Generic;
+using LostAndFoundTracker.Data;
+using LostAndFoundTracker.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace LostAndFoundTracker.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Landing()
         {
-            if (HttpContext.Session.GetString("UserId") != null)
+            if (HttpContext.Session.GetInt32("UserId") != null)
             {
                 return RedirectToAction("Dashboard");
             }
@@ -18,7 +28,7 @@ namespace LostAndFoundTracker.Controllers
 
         public IActionResult Dashboard()
         {
-            if (HttpContext.Session.GetString("UserId") == null)
+            if (HttpContext.Session.GetInt32("UserId") == null)
             {
                 return RedirectToAction("Login", "Account");
             }
@@ -26,24 +36,32 @@ namespace LostAndFoundTracker.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetDashboardData()
+        public async Task<IActionResult> GetDashboardData()
         {
-            var userId = HttpContext.Session.GetString("UserId");
+            int? userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                return Json(new { lostCount = 0, foundCount = 0, resolvedCount = 0, myItemsCount = 0, recentItems = new object[0] });
+            }
 
-            var lostCount = ItemsController.items.Count(i => i.Type == "lost" && !i.IsResolved);
-            var foundCount = ItemsController.items.Count(i => i.Type == "found" && !i.IsResolved);
-            var resolvedCount = ItemsController.items.Count(i => i.IsResolved);
-            var myItemsCount = ItemsController.items.Count(i => i.UserId.ToString() == userId);
+            var lostCount = await _context.Items.CountAsync(i => i.Type == "lost" && !i.IsResolved);
+            var foundCount = await _context.Items.CountAsync(i => i.Type == "found" && !i.IsResolved);
+            var resolvedCount = await _context.Items.CountAsync(i => i.IsResolved);
+            var myItemsCount = await _context.Items.CountAsync(i => i.UserId == userId.Value);
 
-            var recentItems = ItemsController.items.OrderByDescending(i => i.Date)
-                                   .Take(6)
-                                   .Select(i => new {
-                                       id = i.Id,
-                                       name = i.Name,
-                                       location = i.Location,
-                                       date = i.Date.ToString("MMM dd, yyyy"),
-                                       type = i.Type
-                                   }).ToList();
+            var recentItems = await _context.Items
+                .Where(i => !i.IsResolved)
+                .OrderByDescending(i => i.Date)
+                .Take(6)
+                .Select(i => new
+                {
+                    id = i.Id,
+                    name = i.Name,
+                    location = i.Location,
+                    date = i.Date.ToString("MMM dd, yyyy"),
+                    type = i.Type
+                })
+                .ToListAsync();
 
             return Json(new
             {
