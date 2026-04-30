d03a19a21ff8e0b6b7c40e318a1e0da5e79c7d76
Klient Danielle Necesario <necesarioklientdanielle@gmail.com>
2026-05-01 03:01:04 +0800

    feat(db): add StarTransaction and Certificate DbSets with relationships

--- LostAndFoundTracker/Data/AppDbContext.cs
+++ LostAndFoundTracker/Data/AppDbContext.cs
@@ -13,6 +13,8 @@ namespace LostAndFoundTracker.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<Notification> Notifications { get; set; }
+        public DbSet<StarTransaction> StarTransactions { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -39,6 +41,32 @@ namespace LostAndFoundTracker.Data
                 .HasOne(n => n.Item)
                 .WithMany()
                 .HasForeignKey(n => n.ItemId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // StarTransaction relationships
+            modelBuilder.Entity<StarTransaction>()
+                .HasOne(s => s.Receiver)
+                .WithMany()
+                .HasForeignKey(s => s.ReceiverId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<StarTransaction>()
+                .HasOne(s => s.Giver)
+                .WithMany()
+                .HasForeignKey(s => s.GiverId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<StarTransaction>()
+                .HasOne(s => s.Item)
+                .WithMany()
+                .HasForeignKey(s => s.ItemId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Certificate relationships
+            modelBuilder.Entity<Certificate>()
+                .HasOne(c => c.User)
+                .WithMany()
+                .HasForeignKey(c => c.UserId)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
